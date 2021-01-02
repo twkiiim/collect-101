0ef810ab5707af151d1d0aca32e710f2cca59cfa
Taewoo Kim <twkiiim@gmail.com>
2021-01-03 03:43:42 +0900

    search feature impl.

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService, SearchableEntrySortableFields, SearchableSortDirection } from 'src/app/services/API.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  results = [];
 
-  constructor() { }
+  constructor(
+    private apiService: APIService,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  search(keyword) {
+    
+    const sort = {
+      field: SearchableEntrySortableFields.phrase,
+      direction: SearchableSortDirection.desc
+    }
+    this.apiService.SearchEntrys({
+      or: [
+        {
+          phrase: {
+            match: `${keyword}`
+          }
+        },
+        {
+          definition: {
+            match: `${keyword}.*`
+          }
+        },
+        {
+          example: {
+            match: `${keyword}.*`
+          }
+        },
+      ]
+    }, sort).then(results => {
+      this.results = results.items;
+    });
   }
 
 }
